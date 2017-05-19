9d3baa4c4f7759bd961eb24bae6f9aa542f327f2
Brandon Fuller <bjcfuller@uri.edu>
2017-05-19 09:39:36 -0400

    Category selects can now be specifically ordered

--- uri-program-finder.php
+++ uri-program-finder.php
@@ -139,7 +139,7 @@ function uri_program_finder_get_program_category_id() {
 		}
 	}
 
-	print_r($parent);
+	//print_r($parent);
 
 	return $parent;
 
@@ -202,8 +202,22 @@ function uri_program_finder_shortcode($attributes, $content, $shortcode) {
 
 	ob_start();
 	
-	echo '<div class="program-finder">';
-	$categories = uri_program_finder_get_children(0);
+    // get top-level categories and sort 'em
+    $categories = uri_program_finder_get_children(0);
+    $catorder = array(
+        'program-type',
+        'interest-area',
+        'college',
+        'location'
+    );
+    usort($categories, function ($a, $b) use ($catorder) {
+        $pos_a = array_search($a['slug'], $catorder);
+        $pos_b = array_search($b['slug'], $catorder);
+        return $pos_a - $pos_b;
+    });
+    
+    // build the form
+    echo '<div class="program-finder">';
 	foreach($categories as $c) {
 		echo '<form action="' . get_site_url() . '" method="GET" class="program-finder-nojs">';
 		echo '<fieldset>';
