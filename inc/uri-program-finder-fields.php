40cb03f1c039a06e03491e67f2319cff82aa1f4b
John Pennypacker <jpennypacker@uri.edu>
2018-02-24 11:36:51 -0500

    updated program icon, added categories and taxonomies

--- inc/uri-program-finder-fields.php
+++ inc/uri-program-finder-fields.php
@@ -1,4 +1,88 @@
 <?php
+/**
+ * Define the custom post type, its taxonomy, and its fields.
+ */
+
+
+/**
+ * Declare the custom post type
+ */
+
+function uri_program_finder_post_type_maker() {
+
+	register_post_type('program', array(
+		'label' => 'Program',
+		'description' => 'Majors, etc.',
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'capability_type' => 'post',
+		'hierarchical' => true,
+		'rewrite' => array('slug' => 'program'),
+		'query_var' => true,
+		'has_archive' => true,
+		'exclude_from_search' => false,
+		'supports' => array('title','editor','excerpt','thumbnail','revisions','author'),
+		'taxonomies' => array('post_tag', 'category', 'careers' ),
+		'labels' => array (
+			'name' => 'Program',
+			'singular_name' => 'Programs',
+			'menu_name' => 'Programs',
+			'add_new' => 'Add Program',
+			'add_new_item' => 'Add New Program',
+			'edit' => 'Edit',
+			'edit_item' => 'Edit Program',
+			'new_item' => 'New Program',
+			'view' => 'View Program',
+			'view_item' => 'View Program',
+			'search_items' => 'Search Program',
+			'not_found' => 'No Programs Found',
+			'not_found_in_trash' => 'No Programs Found in Trash',
+			'parent' => 'Parent Program',
+		),
+		'menu_icon'   => 'dashicons-welcome-learn-more',
+	));
+
+
+
+	$taxonomy_args = array(
+		'hierarchical' => false,
+		'labels' => array(
+			'name' => _x( 'Careers', 'taxonomy general name', 'uri' ),
+			'singular_name' => _x( 'Career', 'taxonomy singular name', 'textduriomain' ),
+			'search_items' => __( 'Search Careers', 'uri' ),
+			'popular_items' => __( 'Popular Careers', 'uri' ),
+			'all_items' => __( 'All Careers', 'uri' ),
+			'parent_item' => null,
+			'parent_item_colon' => null,
+			'edit_item' => __( 'Edit Career', 'uri' ),
+			'update_item' => __( 'Update Career', 'uri' ),
+			'add_new_item' => __( 'Add New Career', 'uri' ),
+			'new_item_name' => __( 'New Career Name', 'uri' ),
+			'separate_items_with_commas' => __( 'Separate careers with commas (like tags)', 'uri' ),
+			'add_or_remove_items' => __( 'Add or remove careers', 'uri' ),
+			'choose_from_most_used' => __( 'Choose from the most used careers', 'uri' ),
+			'not_found' => __( 'No careers found.', 'uri' ),
+			'menu_name' => __( 'Careers', 'uri' ),
+		),
+		'show_ui' => true,
+		'show_admin_column' => true,
+		'query_var' => true,
+		'rewrite' => array('slug' => 'career'),
+		'singular_label' => 'Career'
+	);
+	
+	register_taxonomy('careers', array( 'program' ), $taxonomy_args);
+
+
+}
+add_action('init', 'uri_program_finder_post_type_maker');
+
+
+
+/**
+ * Define the custom fields
+ */
 
 if(function_exists("register_field_group"))
 {
